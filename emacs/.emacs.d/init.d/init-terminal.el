0051c452689581d3b070e29be68a3a4b997e0b9b
Marc Schlienger <marc.schlienger@posteo.de>
2026-09-26 12:45:19 +0200

    Bind Ghostel terminal shortcuts

--- emacs/.emacs.d/init.d/init-terminal.el
+++ emacs/.emacs.d/init.d/init-terminal.el
@@ -3,8 +3,10 @@
 (use-package ghostel
   :ensure t
   :commands ghostel
-  :bind (:map ghostel-semi-char-mode-map
-              ("C-s" . consult-line))
+  :bind (("C-c t" . ghostel)
+         ("C-c T" . ghostel-project)
+         :map ghostel-semi-char-mode-map
+         ("C-s" . consult-line))
   :config
   (add-to-list 'ghostel-eval-cmds
                '("magit-status-setup-buffer" magit-status-setup-buffer)))
